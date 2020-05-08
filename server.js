2d467b638f6ae75a65be90dd7fe3fa26481a1157
Rodrigo De La Mora <delamorarodrigo3141@yahoo.com>
2020-05-08 08:35:41 -0700

    Started up server in index.js, created server.js and started it up, made scripts for nodemon and node, made project and action routers and filled the framework for them.

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const projectRouter = require("./data/helpers/projectRouter");
+const actionRouter = require("./data/helpers/actionRouter");
+
+const server = express();
+
+server.use(express.json());
+
+server.get("/", logger, (req, res) => {
+    res.send(`<h2>Let's write some middleware!</h2>`)
+})
+
+server.use("/api/projects", logger, projectRouter);
+server.use("/api/actions", logger, actionRouter);
+
+function logger(req, res, next) {
+    console.log(`[${new Date().toISOString()}] ${req.method} to ${req.url}`);
+    next();
+}
+
+module.exports = server;
